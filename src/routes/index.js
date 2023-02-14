056d78fbc1028a999e8b1aa0eac8d7a50f25c763
Alejuda <ignacio.fino@mi.unc.edu.ar>
2023-02-14 15:37:25 -0300

    first commit

---
+++ src/routes/index.js
@@ -0,0 +1,69 @@
+const { Router } = require('express');
+const router = Router();
+const fs = require('fs');
+
+const tasks = require('../tasks.json');
+
+
+router.get('/', (req,res)=>{
+    res.json(tasks)
+});
+
+
+router.post('/', (req, res)=>{
+    const { title, description} = req.body;
+
+    if (title && description){
+        const id = tasks.length + 1;
+        const newTask = {...req.body, id } 
+        tasks.push(newTask);
+        fs.writeFileSync('./src/tasks.json',JSON.stringify(tasks));
+        res.json(tasks);
+        
+    } else {
+    res.status(500).json({error:'Wrong request'});
+    }
+});
+
+
+router.delete('/:id', (req, res)=>{
+    let found = false;
+    tasks.forEach((task, idx)=>{
+        if (task.id == req.params.id) {
+            tasks.splice(idx, 1); 
+            found = true;
+            fs.writeFileSync('./src/tasks.json',JSON.stringify(tasks));
+        }
+    });
+    if (found) {
+        res.json(tasks);
+    } else {
+        res.status(500).json({error: "id not found"})
+    }
+    
+});
+
+
+router.put('/:id', (req, res)=>{
+    const {id} = req.params
+    const { title, description} = req.body;
+
+    if (title && description){
+        tasks.forEach((task, idx)=>{
+            if (task.id == id) {
+                task.title = title;
+                task.description = description;
+                
+            }
+        });
+        fs.writeFileSync('./src/tasks.json',JSON.stringify(tasks));
+        res.json(tasks);
+    } else {
+    res.status(500).json({error:'Wrong request'});
+    }
+
+});
+
+
+
+module.exports = router;
